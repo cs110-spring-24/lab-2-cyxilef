4db8d4190c848dc63f1abcc5d0fcbe9cc99a8599
cyxilef <157657991+cyxilef@users.noreply.github.com>
2024-02-07 17:31:46 -0800

    Number Guessing Game

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -1,12 +1,23 @@
 import random
 num = random.randint(1, 100)
+user = 0
+min = 1
+max = 100
+count = 0
+while user != num:  # users number does not = #
 
-user = input("Enter your guess: ")
-user = int(user)
+    user = input(f"Enter your guess between {min} and {max}: ")
+    user = int(user)
 
-if user > num:
-    print("Too high, it was", num)
-elif user < num:
-    print("Too low, it was", num)
-else:
-    print("You got it!")
+    if user > num:
+        print("Too high")
+        max = user
+    elif user < num:
+        print("Too low")
+        min = user
+    else:
+        print("You got it!")
+
+    count = count + 1
+print(f'It took you {count} guesses to solve it! ')
+# indentations matter
